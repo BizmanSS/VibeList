f96f90eb5f52ddb116a4875257660478f969040a
Dhruvi Patel <dhruvi24.patell@gmail.com>
2025-11-15 15:18:03 -0500

    Moved hardcoded secrets into app.json

--- services/firebase.ts
+++ services/firebase.ts
@@ -2,14 +2,17 @@ import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
+import Constants from "expo-constants";
+
+const extra = Constants.expoConfig?.extra!;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCYCbTS6DWZluFirPHrex2eJ6hsXl3JP5w",
-  authDomain: "vibelist-59e14.firebaseapp.com",
-  projectId: "vibelist-59e14",
-  storageBucket: "vibelist-59e14.firebasestorage.app",
-  messagingSenderId: "354654311889",
-  appId: "1:354654311889:web:fe64edb087714280ecbcf0"
+  apiKey: extra.API_KEY,
+  authDomain: extra.AUTH_DOMAIN,
+  projectId: extra.PROJECT_ID,
+  storageBucket: extra.STORAGE_BUCKET,
+  messagingSenderId: extra.MESSAGING_SENDER_ID,
+  appId: extra.APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
